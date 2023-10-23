e36bed8de80d31951acc4aa69e5fb829c61d3358
Arantza Castro <ary.castro.dessavre@gmail.com>
2023-10-23 17:26:57 +0200

    Initial Commit

---
+++ MC1LunarQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC1LunarQuest
+//
+//  Created by Arantza Castro Dessavre on 23/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC1LunarQuest/MC1LunarQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1LunarQuestApp.swift
+//  MC1LunarQuest
+//
+//  Created by Arantza Castro Dessavre on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MC1LunarQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
